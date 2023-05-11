32a9de94ea2ff0be681fe86c637550ca174a827e
Alvaro del Castillo <alvaro.delcastillo@cobee.io>
2023-05-11 17:50:06 +0200

    Add missing GD script (mac)

--- godot/mixamo_animation_falls_with_floor.gd
+++ godot/mixamo_animation_falls_with_floor.gd
@@ -3,7 +3,7 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$basic_anim_rigging/AnimationPlayer.play("ArmatureAction")
+	$RigidBody3D/Falling/AnimationPlayer.play("Armature|mixamocom|Layer0") # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
